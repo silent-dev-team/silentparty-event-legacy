1e862fad37dbb588757a1f6a91491fc618f85235
Jan <55839828+Piitschy@users.noreply.github.com>
2022-04-08 23:59:53 +0200

    refactoring

---
+++ ticket-pwa/src/components/HistoryBtn.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-btn 
+    class="btn ma-2"
+    fab
+    :dark="value"
+    @click="$emit('input', !value)"
+  >
+    <v-icon
+      large
+    >mdi-history</v-icon>
+  </v-btn>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  name: 'HistoryBtn',
+  props: {
+    value: {
+      type: Boolean,
+      required: false,
+      default: false
+    }
+  }
+})
+</script>
+
+<style scoped>
+.btn{
+  position: absolute;
+  top: 5px;
+  right: 5px;
+}
+</style>
---
+++ ticket-pwa/src/components/PingBtn.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-btn 
+    class="btn ma-2"
+    style="top: 5px; left: 5px;"
+    :color="value ? 'green' : 'red'"
+    fab
+    dark
+    @click="$emit('click')"
+  >
+    <v-icon
+      large
+    >mdi-access-point</v-icon>
+  </v-btn>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  name: 'PingBtn',
+  props: {
+    value: {
+      type: Boolean,
+      required: false,
+      default: false
+    }
+  }
+})
+</script>
+
+<style scoped>
+.btn{
+  position: absolute;
+  top: 5px;
+  left: 5px;
+}
+</style>
