ea503c2f204ee1748ba8ab2e8bd6cc1de4334932
Jan <55839828+Piitschy@users.noreply.github.com>
2022-05-13 10:11:56 +0200

    new history comp

---
+++ kasse/src/components/history.vue
@@ -0,0 +1,29 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+  name: "History",
+  data() {
+    return {
+      route: 'history',
+    }
+  },
+  computed: {
+    ...mapState([
+      'api',
+      'orders'
+    ]),
+  },
+  methods: {
+    fetchOrders() {
+      const response = await fetch(this.api+params.route)
+    },
+  }
+}
+</script>
+
+<style>
+
+</style>
