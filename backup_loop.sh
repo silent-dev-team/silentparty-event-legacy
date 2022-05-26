dc355cff9169be18222f0676c1296aa439eb6555
Piitschy <jan-d.pietsch@live.de>
2022-05-26 23:11:22 +0200

    configure backup

--- backup_loop.sh
+++ backup_loop.sh
@@ -1,8 +1,8 @@
 while true
 do
     sleep 30
-    #echo "backup wird durchgeführt..."
-    TIMESTAMP=`date +%Y-%m-%d_%H-%M-%S`
-    cp ./dump.rdb  "/run/media/jan/BACKUP1/dump+$(date +"%Y%m%d-%H%M%S").rdb"
+    echo "backup wird durchgeführt..."
+    cp ./dump.rdb  "/run/media/server/BACKUP1/dump.rbp"
+    cp ./dump.rdb  "/run/media/server/BACKUP1/dump+$(date +"%Y%m%d-%H%M%S").rdb"
 
 done
