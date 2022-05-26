aa610aca293557f7fc2a6d8b27771d8ad00f8bf0
Jan <55839828+Piitschy@users.noreply.github.com>
2022-05-27 00:37:00 +0200

    update backup routine

--- backup_loop.sh
+++ backup_loop.sh
@@ -2,7 +2,7 @@ while true
 do
     sleep 30
     echo "backup wird durchgeführt..."
-    cp ./dump.rdb  "/run/media/server/BACKUP1/dump.rbp"
-    cp ./dump.rdb  "/run/media/server/BACKUP1/dump+$(date +"%Y%m%d-%H%M%S").rdb"
+    cp ./dump.rdb  "/run/media/$USER/BACKUP1/dump.rbp"
+    cp ./dump.rdb  "/run/media/$USER/BACKUP1/dump+$(date +"%Y%m%d-%H%M%S").rdb"
 
 done
